818dbbc2629e4cf7adc9605df20f60f91a1ad3c3
Chad Horohoe <chad.horohoe@radai.com>
2025-05-12 18:25:33 -0700

    bugfix(lambda-promtail): Include function name in TF output

--- tools/lambda-promtail/outputs.tf
+++ tools/lambda-promtail/outputs.tf
@@ -2,3 +2,8 @@ output "lambda_function_arn" {
   description = "The ARN of the Lambda function created to ingest logs to Loki. This is used to configure account-wide Log Subscription Policy which requires the arn of the Lambda function."
   value       = aws_lambda_function.this.arn
 }
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function created to ingest logs to Loki."
+  value       = aws_lambda_function.this.function_name
+}
